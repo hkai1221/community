888eeee45d3b20a16ef4a1cbbbc7232218eecaf1
hkai1221 <1119654491@qq.com>
2019-07-14 18:00:36 +0800

    init repo

---
+++ src/main/java/life/hk1221/community/CommnuityApplication.java
@@ -0,0 +1,13 @@
+package life.hk1221.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommnuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommnuityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/hk1221/community/CommnuityApplicationTests.java
@@ -0,0 +1,16 @@
+package life.hk1221.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommnuityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
