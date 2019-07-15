f94c86faf309a6b6ad9a9f6dbfc80d1ba89cb955
hkai1221 <1119654491@qq.com>
2019-07-15 17:01:46 +0800

    extract github config from code to properties

---
+++ src/main/java/life/hk1221/community/provider/GithubProvider.java
@@ -0,0 +1,51 @@
+package life.hk1221.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.hk1221.community.dto.AccessTokenDTO;
+import life.hk1221.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+//ioc
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println(string);
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + "f6be7b5dd3fb3e502fe373d4aeaa1d3d546ac533")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+
+        }
+        return null;
+    }
+}
+
